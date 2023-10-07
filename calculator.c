48e1bcd2f58d4833694ff9213bb456f6c806ce87
DangQuangVinh3905 <145881865+DangQuangVinh3905@users.noreply.github.com>
2023-10-07 21:17:41 +0700

    calculator.c

--- calculator.c
+++ calculator.c
@@ -1,134 +1,165 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lib.h"
 
-// create a function to extract operands
-int* extract_operand(char* input, int* j){
-    int* numbers = (int*)malloc(100*4);
-    char currentValue[20]; // Assuming a maximum length for the values
-    int currentIndex = 0;
-    bool isNegative = false;
-    *j = 0;
-
-    for (int i = 0; i < strlen(input); i++) {
-        char currentChar = input[i];
-
-        if (isdigit(currentChar) || (currentChar == '-' && (i == 0 || input[i - 1] == ' '))) {
-            // Append the character to the current value
-            currentValue[currentIndex] = currentChar;
-            currentIndex++;
-        } else if (currentIndex > 0) {
-            // Null-terminate the current value and convert it to an integer
-            currentValue[currentIndex] = '\0';
-            int numericValue = atoi(currentValue);
-
-            // Print the numeric value
-            //printf("->Value: %d\n", isNegative ? -numericValue : numericValue);
-
-        if (isNegative){
-            numbers[(*j)++] = -numericValue;
-        }
-        else numbers[(*j)++] = numericValue;
-
-            // Reset the current value
-            currentIndex = 0;
-            isNegative = false;
-        }
-
-        if (currentChar == '-') {
-            isNegative = true;
-        }
+void display_c(char *arr, int size){
+    for (int i = 0; i<size; i++){
+        printf("%c - i = %d\n", arr[i], i);
     }
-
-    // Check if there's a pending value at the end of the string
-    if (currentIndex > 0) {
-        currentValue[currentIndex] = '\0';
-        int numericValue = atoi(currentValue);
-        //printf("------->Value: %d\n", isNegative ? -numericValue : numericValue);
-
-        if (isNegative){
-            numbers[(*j)++] = -numericValue;
-        }
-        else numbers[(*j)++] = numericValue;
-    
-    }
-    return numbers;
 }
 
+void display_n(int *arr, int size){
+    for (int i = 0; i<size; i++){
+        printf("%d\n", arr[i]);
+    }
+}
 
-// create a function to extract operators
-char* extract_operator(const char *msg, int*j){
-    char* list = (char*)malloc(100*1);
-    *j =0;
-    for (int i = 0; msg[i]!='\0'; i++){
-        char c = msg[i];
-        if(c == '+'){
-            list[(*j)++] = c;
+void getOperands(char* msg, int size, int* list, int* total){
+    char anti_space[size];
+    int j = 0;
+    // anti space
+    for (int i = 0; i<size; i++){
+        if (msg[i] == '+' || msg[i] == '-' || isdigit(msg[i]) == 1 || msg[i] == '*' || msg[i] == '/'){
+            anti_space[j++] = msg[i];
         }
-        if(c == '-' && msg[i-1] != '*'){
-            if (msg[i-1] != '+' && msg[i-1] != '*' && msg[i-1] != '/'){
-                list[(*j)++] = '.';
+    }
+
+
+    int number_lists[j];
+    int check = 0, index = 0, column = 0;
+
+    char tmp[j][j];
+    int num;
+    // get the number in the string
+    for (int i =0; i<j; i++){
+        tmp[column][index++] = anti_space[i];
+        
+        if (isdigit(anti_space[i]) == 1 && isdigit(anti_space[i+1]) != 1){
+            tmp[column][index] = '\0';
+            index = 0;
+            column++;
+        }
+    }
+
+    // chose '+' or '-'
+    int column1 = 0;
+    int number_of_operators = 0;
+    char operators[column][j];
+
+    int list_number_of_operators[column];
+    int list_number_of_operands[column];
+    char list_characters_of_operands[column][20];
+    int list_characters_of_operands_index = 0;
+    for (int i = 0; i<column; i++){
+        number_of_operators = 0;
+        list_characters_of_operands_index = 0;
+        for (int i1 = 0; i1<strlen(tmp[i]); i1++){
+            // if the number of '-' is even --> '+'
+            if (tmp[i][i1] == '-'){
+                number_of_operators++;
             }
-
-        }  
-
-        if(c == '*'){
-            list[(*j)++] = c;
+            // get the number char
+            if (isdigit(tmp[i][i1])){
+                list_characters_of_operands[i][list_characters_of_operands_index++] = tmp[i][i1];
+            }
         }
-        if(c == '/'){
-            list[(*j)++] = c;
-        } 
+        // null terminator
+        list_characters_of_operands[i][list_characters_of_operands_index] = '\0';
+
+        list_number_of_operands[i] = atoi(list_characters_of_operands[i]);
+        list_number_of_operators[i] = number_of_operators;
     }
-    return list;
+
+
+    for (int i = 0; i<column; i++){
+        if (list_number_of_operators[i] %2 == 1){
+            list_number_of_operands[i] = -list_number_of_operands[i];
+        }
+    }
+
+    *total = column;
+    for (int i = 0; i<column; i++){
+        list[i] = list_number_of_operands[i];
+    }
 }
 
-// calculate numbers with 4 standard operators
-int four_operators(char* msg){
-    int count=0, count1=0;
-    int* list_num = extract_operand(msg, &count);
-    char* list_c = extract_operator(msg, &count1);
+void getOperators(char* msg, int size, char* list, int* total){
+    char anti_space[size];
+    int index = 0;
+    for (int i = 0; i<size; i++){
+        if (msg[i] == '+' || msg[i] == '-' || isdigit(msg[i]) == 1 || msg[i] == '*' || msg[i] == '/'){
+            anti_space[index++] = msg[i];
+        }
+    }
 
-    int res;
-    int i=0;
-    int check = 0;
+    char operators[100];
+    int j = 0;
+    for (int i = 0; i<size; i++){
+        if (isdigit(anti_space[i-1]) && (anti_space[i] == '+' || anti_space[i] == '-')){
+            operators[j++] = '+';
+        }
+        else if (isdigit(anti_space[i-1]) && (anti_space[i] == '*' || anti_space[i] == '/')){
+            operators[j++] = anti_space[i];
+        }
+    }
+    *total = j;
+    for (int i = 0; i<j; i++){
+        list[i] = operators[i];
+    }
+}
 
-    while(i<(count1)){
-        if (list_c[i] == '*' || list_c[i] == '/'){
-            int num1 = list_num[i];
-            int num2 = list_num[i+1];
-            char operator = list_c[i];
-      
-            if (operator == '*'){
+int normal_calculations(char* msg, int size){
+    int n_operators, n_operands;
+    int operands[20];
+    char operators[20];
+    getOperators(msg, size, operators, &n_operators);
+
+    getOperands(msg, size, operands, &n_operands);
+
+    for (int i = 0; i<n_operands; i++){
+       printf("%d; ", operands[i]);
+    }
+    printf("\n");
+    for (int i = 0; i<n_operators; i++){
+       printf("%c; ",operators[i]);
+    }
+    printf("\n------------\n");
+
+    int i = 0, res;
+    int num1, num2;
+    while(i < n_operators){
+        if (operators[i] == '*' || operators[i] == '/'){
+            num1 = operands[i];
+            num2 = operands[i+1];
+            char op = operators[i];
+
+            if (op == '*'){
                 res = num1*num2;
             }
-            else {
+            else if (op == '/'){
                 res = num1/num2;
             }
-
-            list_num[i+1] = res;
-            list_num[i] = -222; // nothing
-            list_c[i] = 'x'; // nothing
-
-            check++;
+            //printf("--> %d, n1[%d] = %d, n2[%d] = %d\n", res, i, num1, i+1, num2);
+            operands[i+1] = res;
+            operands[i] = -222; // nothing
+            operators[i] = 'f'; // nothing
         }
         i++;
     }
 
     i = 0;
-    while(i<(count1)){
-        if (list_c[i] == '+' || list_c[i] == '-'){
-            int num1 = list_num[i];
+    while(i < n_operators){
+        if (operators[i] == '+' || operators[i] == '-'){
+            num1 = operands[i];
             int state = i;
             // ignore 'x' until get '+' or '-' on list_c
-            while(list_c[state+1] == 'x'){
+            while(operators[state+1] == 'f'){
                 state++;
             }
-            int num2 = list_num[state+1];
-            char op = list_c[i];
-
+            num2 = operands[state+1];
+            char op = operands[i];
 
             // num1 and num2 are probably negative numbers
             if (op == '+'){
@@ -137,129 +168,64 @@ int four_operators(char* msg){
                 res = num1+num2;
             }
 
-            // save te result into list_num to replace the index of num2
-            list_num[state+1] = res;
-        } 
+            //printf("--> %d, n1[%d] = %d, n2[%d] = %d\n", res, i, num1, state+1, num2);
+            operands[state+1] = res;
+        }
         i++;
     }
-    free(list_c);
-    free(list_num);
-    return res;
- 
 
+    return res;
 }
 
-// extract and replace equations having brackets
-void sub_string(char* msg, char* msg_res){
-    int len = strlen(msg);
-    int flag=0;
-    int index;
-    char list[10][2][200];
-    int j=0;
+void sub(char *msg, int size){
+    int flag = 0;
+    char list[10][size];
+    int string_i=0, char_i=0;
 
-    char new[200];
-    for (int i = 0; i<len+1; i++){
+    for (int i = 0; i<size; i++){
         if (msg[i] == '('){
             flag = 1;
-            index = 0;
+            //list[string_i][char_i++] = msg[i];
         }
-
-        // if encountering ')', stop adding elements
-        if (msg[i-1] == ')' && flag == 1){
-            flag = -1;
-            new[index] = '\0';
-
-
-            char new1[200];
-            int len = strlen(new);
-            memmove(new1+1, new, len+1);
-
-            // add '0' to new1 to express 0-1 = -1 or 01 = 1
-            new1[1] = '0';
-            new1[0] = '(';
-            new1[len+1] = '\0';
-    
-
-            
-            memmove(list[j][0], new1, strlen(new1)+3);
-
-            // replace the passage with new1's sub_string
-            REPLACE_PASS(msg, new, new1, msg);
-
-            int res = four_operators(list[j][0]);
-
-            // if result <0 --> add '1' to express 1*-1 = -1 
-            char result[50];
-            if (res < 0){
-                sprintf(result, "1*%d", res);
-            }
-            else sprintf(result, "%d", res);
-
-            // copy result 's string to list[j][0] which is the string needed to be replaced
-            memmove(list[j][1], result, strlen(result)+1);
-            list[j][1][strlen(result)] = '\0';
-
-            j++;
+        else if (msg[i] == ')'){
+            list[string_i][char_i++] = msg[i];
+            list[string_i++][char_i] = '\0';
+            char_i=0;
+            flag = 0;
+            //printf("list[string_i-1] = %s\n", list[string_i-1]);
         }
-
-        // if encountering '(', start adding elements
         if (flag == 1){
-            new[index] = msg[i];
-            index++;
-        }
-
-    }
-
-    for (int i = 0; i<j; i++){
-        REPLACE_PASS(msg, list[i][0], list[i][1], msg_res);
-    }
-
-}
-
-// check if equations having brackets
-int isHavingBracket(char* msg){
-    int len = strlen(msg);
-    for (int i = 0; i<len+1; i++){
-        if (msg[i] == '('){
-            return 1;
+            list[string_i][char_i++] = msg[i];
         }
     }
-    return 0;
+
+    char new[size];
+    strcpy(new, msg);
+    for (int i = 0; i < string_i; i++){
+        int tmp = normal_calculations(list[i], strlen(list[i])+1);
+        char temp[100];
+        sprintf(temp, "%d", tmp);
+        //printf("string = %s\n", temp);
+        //memmove();
+        
+        REPLACE_PASS(new, list[i], temp, new);
+        //printf("new = %s\n", new);
+    }
+
+    int tmp = normal_calculations(new, strlen(new)+1);
+    printf("%s = %d\n", msg, tmp);
 }
 
-// print calculations on termina;
-void printTerminal(char* msg, int res){
-    printf("**********************\n");
-    printf("%s = %d\n", msg, res);
-    printf("**********************\n");
-}
-
-int main(){ 
+int main(){
+    //char msg[] = "(10*2-10)*2 + 5*2*(4/2)";
+    //char msg[] = "(1+2)*2  + 3* -2 +4*(20/5)";
     while(1){
-    char msg[200];
-    char msg1[200];
-    printf("enter: ");
+        printf("Enter the calculation formula: ");
+        char msg[200];
+        fgets(msg, 200, stdin);
+        msg[strlen(msg)-1] = '\0';
 
-    fgets(msg1, sizeof(msg1), stdin);
-    int len = strlen(msg1);
-    msg1[len-1] = '\0';
-    
-    // add '0' to msg to express 0-1 = -1
-    memmove(msg+1, msg1, len+1);
-    msg[0] = '0';
-    msg[len] = '\0';
-
-
-
-    if (isHavingBracket(msg) == 0){
-        printTerminal(msg1, four_operators(msg));
-    }
-    else if (isHavingBracket(msg) == 1){
-        char msg_result[200];
-        sub_string(msg, msg_result);
-        int count, count1;
-
-        printTerminal(msg1, four_operators(msg_result));
-    }
+        int size = strlen(msg)+1;
+        sub(msg, size);
     }
 }
